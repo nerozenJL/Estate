c99b93fc82358ceeda44fce7a94548b8fb18d707
nerozenJL <nerozenJL@gmail.com>
2016-05-31 17:57:57 +0800

    ready to merge 2

--- app/build/generated/source/r/debug/com/handmark/pulltorefresh/library/R.java
+++ app/build/generated/source/r/debug/com/handmark/pulltorefresh/library/R.java
@@ -42,11 +42,11 @@ public final class R {
 		public static final int indicator_right_padding = 0x7f08001b;
 	}
 	public static final class drawable {
-		public static final int default_ptr_flip = 0x7f020073;
-		public static final int default_ptr_rotate = 0x7f020074;
-		public static final int indicator_arrow = 0x7f02009c;
-		public static final int indicator_bg_bottom = 0x7f02009d;
-		public static final int indicator_bg_top = 0x7f02009e;
+		public static final int default_ptr_flip = 0x7f020074;
+		public static final int default_ptr_rotate = 0x7f020075;
+		public static final int indicator_arrow = 0x7f02009d;
+		public static final int indicator_bg_bottom = 0x7f02009e;
+		public static final int indicator_bg_top = 0x7f02009f;
 	}
 	public static final class id {
 		public static final int both = 0x7f0c001b;
